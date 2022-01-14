ae16cfdaa7760f881d71d9f3477a2b26e6d9eefd
Vladimir I <vladimir.ilmov@jetbrains.com>
2022-01-14 02:36:50 +0100

    5

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String s = "1";
-        System.out.println(s.isBlank());
+        System.out.println(s);
 
         String d = "3";
     }
